c212a0d38d95b1eb94bc7ee5a2001cefdd72b90a
martincochepat <martin.cochepat@gmail.com>
2026-06-09 21:07:41 +0200

    Update test-api-football.js

--- api/test-api-football.js
+++ api/test-api-football.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   try {
     const response = await fetch(
-      "https://v3.football.api-sports.io/leagues?id=1",
+      "https://v3.football.api-sports.io/fixtures?league=1&season=2026",
       {
         headers: {
           "x-apisports-key": process.env.API_FOOTBALL_KEY,
@@ -10,7 +10,11 @@ export default async function handler(req, res) {
     );
 
     const data = await response.json();
-    res.status(200).json(data);
+    res.status(200).json({
+      results: data.results,
+      first: data.response?.[0],
+      sample: data.response?.slice(0, 5)
+    });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
