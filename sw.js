5e9ca832e6c30dbe1d10b77f784807a67514ee19
martincochepat <martin.cochepat@gmail.com>
2026-06-09 08:56:09 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='guide-mondial-2026-v25';
+const CACHE_NAME='guide-mondial-2026-v26';
 self.addEventListener('install',e=>{self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)));await self.clients.claim();})());});
 self.addEventListener('fetch',()=>{});
